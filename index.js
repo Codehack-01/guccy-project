c4f584b40c1150143582fb2fbe9cd8e4d8f8e095
Codehack <ademolaprojectdreams@gmail.com>
2023-09-05 03:44:28 +0100

    source code

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-var carouselWidth = $('.carouselinner-shop')[0].scroll-Width;
-var cardWith = $('.carouselitem-shop').width();
+var carouselWidth = $('.carousel-inner')[0].scroll-Width;
+var cardWith = $('.carousel-item').width();
 
 var scrollPosition = 0;
-$('.carousel-control-next-shop').on('click', function(){
+
+
+$('.carousel-control-next').on('click', function(){
     if(scrollPosition < (carouselWidth - (cardWidth * 4))){
         console.log('next');
         scrollPosition = scrollPosition + cardWidth;
-        $('.carouselinner-shop').animate({scrollLeft:
+        $('.carousel-inner').animate({scrollLeft:
         scrollPosition},600);
     }
 });
